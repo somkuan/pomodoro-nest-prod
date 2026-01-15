c769678e9be0aa813776dacd4814be7f368e2788
somkuan <somkuan@gmail.com>
2026-01-15 10:44:58 +0700

    Authen 1

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  register(): { msg: string } | PromiseLike<{ msg: string }> {
+    return { msg: 'Error message' };
+  }
+}
