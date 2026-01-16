69eee430282175c2e98d853f86f9608deef01b4b
somkuan <somkuan@gmail.com>
2026-01-16 12:23:01 +0700

    config DB pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,41 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+      envFilePath: '.env', // Path to your .env file
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      // eslint-disable-next-line @typescript-eslint/require-await
+      useFactory: async (config: ConfigService) => {
+        // console.log('Database configuration:', {
+        //   host: config.get<string>('DB_HOST'),
+        //   port: config.get<number>('DB_PORT'),
+        //   username: config.get<string>('DB_USERNAME'),
+        //   database: config.get<string>('DB_DATABASE'),
+        // });
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: +(config.get<number>('DB_PORT') ?? 5432),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true, // Set to false in production
+        };
+      },
+      inject: [ConfigService],
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthService {
   register(): { msg: string } | PromiseLike<{ msg: string }> {
-    return { msg: 'Error message' };
+    return { msg: 'Auth Success' };
   }
 }
